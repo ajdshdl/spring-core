2f28855c5b690901a3aa8d7b47c64cc0c6155117
kdh <ajdshdl@naver.com>
2023-03-09 16:05:32 +0900

    AppConfig  refactoring

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,19 +1,38 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderServiceImpl;
-
+//after refactoring
 public class AppConfig {
 
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
-
     public OrderServiceImpl orderService(){
-        return new OrderServiceImpl( new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
+    }
 }
+// before refactoring
+//public class AppConfig {
+//
+//    public MemberService memberService(){
+//        return new MemberServiceImpl(new MemoryMemberRepository());
+//    }
+//
+//    public OrderServiceImpl orderService(){
+//        return new OrderServiceImpl( new MemoryMemberRepository(), new FixDiscountPolicy());
+//    }
+//
+//}
