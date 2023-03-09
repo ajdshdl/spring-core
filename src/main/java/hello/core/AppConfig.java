ff5a81ffda7c2ac5d7b4a35bf0a85eef036a0fd0
kdh <ajdshdl@naver.com>
2023-03-09 17:30:34 +0900

    convert spring

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,27 +1,32 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderServiceImpl;
-//after refactoring
-public class AppConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+public class AppConfig { //after refactoring
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
+    @Bean
     public OrderServiceImpl orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public DiscountPolicy discountPolicy(){
-        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
 // before refactoring
